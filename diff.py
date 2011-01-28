5e4b43b8de8c3df8630a7cbdf42c73304ca6161f
Genas2 <Gennas2@gmail.com>
2011-01-28 09:02:17 +0200

    Calculate differentials of higher degrees now

--- diff.py
+++ diff.py
@@ -2,10 +2,19 @@
 
 import math
 
-def diff(func, x, l, prec=0.01):
-  return (func(x + prec, l) - func(x - prec, l))/(2 * prec)
+def diff(func, x, l, exponent=0, prec=0.01):
+  if exponent == 0:
+   return func(x, l)
+  elif exponent < 0:
+   return -1
+  elif exponent == 1:
+   return (func(x + prec, l) - func(x - prec, l))/(2 * prec)
+
+  while exponent > 1:
+   expontent = exponent - 1
+   result = (diff(func, x + prec, l, exponent, prec) - diff(func, x - prec, l, exponent, prec))/(2 * prec) 
 
 func = lambda x, l: math.pow((math.cos(x) * math.cos(x) - 1), l)
 
-diff(func, math.pi / 2, 1)
+print(diff(func, math.pi / 4, 2, 1))
 
