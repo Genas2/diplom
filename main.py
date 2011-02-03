ccb0bc89cf02cd48908fafd5e065c8c63b0405af
Genas2 <Gennas2@gmail.com>
2011-02-03 08:58:37 +0200

    Small changes

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+import subprocess
+
+MAXIMA_FILE = 'equas.mac'
+
+fd = open(MAXIMA_FILE, 'r')
+
+comment = False
+start_comment = -1
+end_comment = -1
+
+for line in fd:
+  
+  start_comment = line.find('/*')
+  end_comment = line.find('*/')
+
+  if start_comment != -1:
+    #comment = False
+    start_comment = line.find('/*')
+
+    if find('/*') != -1:
+      comment = True
+
+
+  if comment != True:
+    print(line)
+
+fd.close()
