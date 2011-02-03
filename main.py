8eb07a4827c251d077fec3a95313816d35350d9a
Genas2 <Gennas2@gmail.com>
2011-02-03 12:13:39 +0200

    Created comment exclution for maxima code in main

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python
 
 import subprocess
+import re
 
 MAXIMA_FILE = 'equas.mac'
 
 fd = open(MAXIMA_FILE, 'r')
 
-comment = False
-start_comment = -1
-end_comment = -1
+comment = 0
+start_comment = 0
+end_comment = 0
 
 for line in fd:
   
-  start_comment = line.find('/*')
-  end_comment = line.find('*/')
+  start_comment = start_comment + len(re.findall('/\*', line))
+  end_comment = end_comment + len(re.findall('\*/', line))
 
-  if start_comment != -1:
-    #comment = False
-    start_comment = line.find('/*')
+  comment = start_comment - end_comment
 
-    if find('/*') != -1:
-      comment = True
-
-
-  if comment != True:
+  if comment < 0:
+      print("Comment syntax error in %s", MAXIMA_FILE)
+  elif end_comment:
+      line = re.sub('/\*.*\*/', '', line)
+  elif comment == 0:
     print(line)
 
 fd.close()
