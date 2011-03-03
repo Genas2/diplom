b30d206dfbf5ab52312bf6cc726c6ca54337c757
Genas2 <Gennas2@gmail.com>
2011-03-03 23:10:15 +0200

    plot.py creates temp .gnuplot file and calls GnuPlot

--- plot.py
+++ plot.py
@@ -1,9 +1,12 @@
 #/usr/bin/python2
 
+# gp in var names means GnuPlot
+
+import os
 import subprocess
 #import equations
 
-gnuplot_settings='''set ticslevel 0
+gp_settings = '''set ticslevel 0
 set style line 100 lt -1 lw 1
 set pm3d hidden3d 100
 set pm3d depthorder
@@ -15,6 +18,19 @@ set xlabel "x"
 set ylabel "y"
 set zlabel "z"
 set datafile missing "NaN"
-splot "-" title "Function" with pm3d'''
+splot "-" title "Function" with pm3d
+'''
+gp_input = open('test.gnuplot', 'r').read()
+gp_filename = 'temp.gnuplot'
+gp_fd = open(gp_filename, 'w')
+gp_fd.write(gp_settings)
+gp_fd.write(gp_input)
+gp_fd.close()
 
+p = subprocess.Popen(['gnuplot', '-p', gp_filename])
+p.wait()
 
+try:
+    os.remove(gp_filename)
+except OSError as (errno, strerror):
+    print(gp_filename + ': ' + strerror)
